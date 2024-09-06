040b60c9118a5c2a4cea3a26664c7e78e03c3c09
dudagncv <125581993+dudagncv@users.noreply.github.com>
2024-09-05 21:38:28 -0300

    Update perguntas.js

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,1 +1,130 @@
 
+export const perguntas =[
+    {
+        enunciado:"Qual gênero musical você mais gosta?",
+         alternativas:[
+            {
+                texto:"Rock Indie"
+                afirmacao:[
+                    "Bôa",
+                    " Você é uma pessoa melancolica e reflexiva."
+
+                ],
+                proxima:1,
+            },
+            {
+                texto:"Rock Alternativo"
+                afirmacao:[
+                    "The Neighbourhood",
+                     "Você é uma pessoa que gosta de experimentar novas sensações."
+                ],
+                proxima:2,
+            },
+            {
+                texto:"Nu Metal"
+                afirmação:[
+                    "Deftones",
+                    "Você é uma pessoa que vive dentro da sua própria cabeça onde tem que lidar com sentimentos avassaladores."
+
+                ],
+                proxima:3,
+            },
+            {
+                texto:"Pop"
+                afirmacao:[
+                  "Chase Atlantic.",
+                   "Você é uma pessoa que gostaria de viver altas aventuras mesmo que se perca no caminho."
+                ],
+                proximo:4,
+            },
+              
+         ] 
+    },
+  {
+    enunciado:"Pra onde você iria se tivesse que fugir ?",
+    alternativas:[
+       {
+           texto:"FLORESTA"
+           afirmacao:[
+               "Lázaro Barbosa",
+               "Não gosta de pessoas é prefere animais é sua própria companhia ",
+               " É o que eu mais me identifico."
+
+           ],
+           proxima:1,
+       },
+       {
+           texto:"PRAIA"
+           afirmacao:[
+               "Alfred Ballí Treviño.",
+               "Gosta de lugar ensolarado e mar",
+                "É o que eu mais me identifico"
+           ],
+           proxima:2,
+       },
+       {
+           texto:"OUTRO PAÍS"
+           afirmação:[
+               "Plabo Escobar",
+               "Fácil de se esconder e ainda conheço outros lugares",
+               "É o que eu mais me identifico"
+
+           ],
+           proxima:3,
+       },
+       {
+           texto:"INTERIOR DE UMA CIDADE"
+           afirmacao:[
+             "Lázaro barbosa.",
+             "Quem vai no fim do mundo me procurar?",
+              "É o que eu mais me identifico."
+           ],
+           proximo:4,
+       },
+         
+    ] 
+},
+{
+    enunciado:"Se você fosse escolher um objeto qual seria ?",
+    alternativas:[
+       {
+           texto:"FACA"
+           afirmacao:[
+               "Lázaro Barbosa.",
+               "É o que tem perto.",
+               " É o que eu mais me identifico."
+
+           ],
+           proxima:1,
+       },
+       {
+           texto:"ARMA"
+           afirmacao:[
+               "Pablo Escobar.",
+               "Mata mais fácil.",
+                "É o que eu mais me identifico"
+           ],
+           proxima:2,
+       },
+       {
+           texto:"ESPADA"
+           afirmação:[
+               "Espadachim",
+               "Se acha o Zoro",
+               "É o que eu mais me identifico"
+
+           ],
+           proxima:3,
+       },
+       {
+           texto:"LUVA COM LÂMINAS AFIADAS"
+           afirmacao:[
+             "Freddy Krueger.",
+             "Acho legal",
+              "É o que eu mais me identifico"
+           ],
+           proximo:4,
+       },
+         
+    ] 
+};
